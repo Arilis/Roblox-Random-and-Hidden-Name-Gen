c345ed51cfbce2b71d5da23b14066c9e612057a9
ArilisDev <32502295+Arilis@users.noreply.github.com>
2020-05-12 09:58:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import requests, random, json, string, os
+from console.utils import set_title
 
 os.system('cls')
+
+def title(text):
+    set_title(text)
 
 def hiddenNameGen():
     length = random.randint(4, 16)
@@ -12,7 +16,17 @@ def regularNameGen():
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for i in range(length))
 
-choice = input('Choose your name generator option!\n(1) Regular Name Generator\n(2) Hidden Name Generator\nChoice: ')
+def lengthNameGen(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
+
+title('Name Generator by ArilisDev | Tried: 0 | Used: 0 | Available: 0')
+
+choice = input('Choose your name generator option!\n(1) Regular Name Generator\n(2) Hidden Name Generator\n(3) Generate by Length\nChoice: ')
+
+tried = 0
+used = 0
+available = 0
 
 if choice == '1':
     while True:
@@ -20,7 +34,14 @@ if choice == '1':
         r = requests.get("https://api.roblox.com/users/get-by-username?username=" + name)
         a = r.text
         if a.find('Id') == -1:
+            tried = tried + 1
+            available = available + 1
             print('[+]: ' + name + ' is available!')
+            title('Name Generator by ArilisDev | Tried: ' + str(tried) + ' | Used: ' + str(used) + ' | Available: ' + str(available))
+        else:
+            tried = tried + 1
+            used = used + 1
+            title('Name Generator by ArilisDev | Tried: ' + str(tried) + ' | Used: ' + str(used) + ' | Available: ' + str(available))
 
 if choice == '2':
     while True:
@@ -28,4 +49,27 @@ if choice == '2':
         r = requests.get("https://api.roblox.com/users/get-by-username?username=" + name)
         a = r.text
         if a.find('Id') == -1:
+            tried = tried + 1
+            available = available + 1
+            title('Name Generator by ArilisDev | Tried: ' + str(tried) + ' | Used: ' + str(used) + ' | Available: ' + str(available))
             print('[+]: ' + name + ' is available!')
+        else:
+            tried = tried + 1
+            used = used + 1
+            title('Name Generator by ArilisDev | Tried: ' + str(tried) + ' | Used: ' + str(used) + ' | Available: ' + str(available))
+
+if choice == '3':
+    length = int(input('How long would you like the username? (4-16): '))
+    while True:
+        name = lengthNameGen(length)
+        r = requests.get("https://api.roblox.com/users/get-by-username?username=" + name)
+        a = r.text
+        if a.find('Id') == -1:
+            tried = tried + 1
+            available = available + 1
+            title('Name Generator by ArilisDev | Tried: ' + str(tried) + ' | Used: ' + str(used) + ' | Available: ' + str(available))
+            print('[+]: ' + name + ' is available!')
+        else:
+            tried = tried + 1
+            used = used + 1
+            title('Name Generator by ArilisDev | Tried: ' + str(tried) + ' | Used: ' + str(used) + ' | Available: ' + str(available))
